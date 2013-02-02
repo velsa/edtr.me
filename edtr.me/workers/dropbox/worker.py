da448c946ea2ff8e7d356024d9c2bb382becc694
st4lk <myhappydo@gmail.com>
2013-02-03 01:55:01 +0400

    refactor DropboxWorkerMixin: move private methods to the end

--- edtr.me/workers/dropbox/worker.py
+++ edtr.me/workers/dropbox/worker.py
@@ -29,64 +29,6 @@ TEXT_MIMES = (
 
 
 class DropboxWorkerMixin(DropboxMixin):
-    def _unify_path(self, path):
-        while len(path) > 1 and path.endswith('/'):
-            path = path[:-1]
-        if len(path) > 1 and not path.startswith('/'):
-            path = '/' + path
-        return path
-
-    def _delta_called_recently(self, user):
-        if user.last_delta:
-            last_delta_time = datetime.now() - user.last_delta
-            if last_delta_time.total_seconds() < DELTA_PERIOD_SEC:
-                return True
-        return False
-
-    @gen.engine
-    def _update_delta_from_dropbox(self, user, callback):
-        # check, that update_delta is not called very often
-        if not self._delta_called_recently(user):
-            # Get delta metadata from dropbox
-            access_token = user.get_dropbox_token()
-            post_args = {}
-            if user.dbox_cursor:
-                post_args['cursor'] = user.dbox_cursor
-            has_more = True
-            cursor = None
-            while has_more:
-                # make dropbox request
-                user.last_delta = datetime.now()
-                # TODO find a way to call save one time in this func
-                yield motor.Op(user.save, self.db)
-                response = yield gen.Task(self.dropbox_request,
-                    "api", "/1/delta",
-                    access_token=access_token,
-                    post_args=post_args)
-                if self._check_bad_response(response, callback):
-                    return
-                dbox_delta = json.loads(response.body)
-                has_more = dbox_delta['has_more']
-                cursor = dbox_delta['cursor']
-                if dbox_delta['reset']:
-                    logger.debug(
-                        "Reseting user all files for '{0}'".format(user.name))
-                    yield motor.Op(self.db[user.name].drop)
-                for e_path, entry in dbox_delta['entries']:
-                    if entry is None:
-                        # TODO
-                        # maybe there is a way to delete files in one db call
-                        yield motor.Op(
-                            DropboxFile.remove_entries, self.db,
-                            {"_id": e_path}, collection=user.name)
-                    else:
-                        # TODO
-                        # maybe there is a way to save files in one db call
-                        yield motor.Op(self._save_meta, entry, user.name, False)
-            user.dbox_cursor = cursor
-            yield motor.Op(user.save, self.db)
-        callback({'status': ErrCode.ok})
-
     @gen.engine
     def wk_dbox_get_tree(self, user, path, recurse=False, callback=None):
         # Update metadata from dropbox to database
@@ -107,47 +49,6 @@ class DropboxWorkerMixin(DropboxMixin):
                 logger.debug(u"FIND_LIST_LEN reached for user '{0}',"
                     " path '{1}".format(user.name, path))
             callback(result)
-
-    def _get_response_encoding(self, response):
-        encoding = 'ascii'
-        cont_type = response.headers.get('Content-Type', None)
-        if cont_type:
-            for v in cont_type.split(';'):
-                if 'charset=' in v:
-                    encoding = v.split('=', 1)[-1]
-                    encoding = DROPBOX_ENCODE_MAP.get(encoding, encoding)
-                    break
-        return encoding
-
-    def _check_bad_response(self, response, callback=None):
-        if response.code != 200:
-            error = 'undefined'
-            if 'error' in response.body:
-                error = json.loads(response.body)['error']
-            result = {
-                "status": ErrCode.bad_request,
-                'http_code': response.code,
-                'error': error,
-            }
-            if callback:
-                callback(result)
-            return result
-        return None
-
-    def _get_file_url(self, path, api_url):
-        path = self._unify_path(path)
-        return "/1/{api_url}/{{root}}{path}".format(
-            api_url=api_url,
-            path=urllib.quote(path.encode('utf8')))
-
-    def _skip_spam_filemeta(self, file_meta, callback):
-        if file_meta and file_meta.last_updated:
-            time_left = datetime.now() - file_meta.last_updated
-            if time_left.total_seconds() < FILE_CONTENT_PERIOD_SEC:
-                callback({
-                    'status': ErrCode.called_too_often})
-                return True
-        return False
 
     @gen.engine
     def wk_dbox_get_file(self, user, path, callback=None):
@@ -218,15 +119,6 @@ class DropboxWorkerMixin(DropboxMixin):
                 'status': ErrCode.ok,
                 'url': url_trans,
                 'expires': url_expires})
-
-    @gen.engine
-    def _save_meta(self, meta_data, colln, update=True,
-      callback=None):
-        meta_data['_id'] = meta_data.pop('path')
-        meta_data['root_path'] = os.path.dirname(meta_data['_id'])
-        if update:
-            meta_data['last_updated'] = datetime.now()
-        self.db[colln].save(meta_data, callback=callback)
 
     @gen.engine
     def wk_dbox_save_file(self, user, path, text_content, callback=None):
@@ -340,3 +232,111 @@ class DropboxWorkerMixin(DropboxMixin):
                 {"_id": from_path}, collection=user.name)
         yield motor.Op(self._save_meta, file_meta, user.name)
         callback({'status': ErrCode.ok})
+
+    def _unify_path(self, path):
+        while len(path) > 1 and path.endswith('/'):
+            path = path[:-1]
+        if len(path) > 1 and not path.startswith('/'):
+            path = '/' + path
+        return path
+
+    def _delta_called_recently(self, user):
+        if user.last_delta:
+            last_delta_time = datetime.now() - user.last_delta
+            if last_delta_time.total_seconds() < DELTA_PERIOD_SEC:
+                return True
+        return False
+
+    @gen.engine
+    def _update_delta_from_dropbox(self, user, callback):
+        # check, that update_delta is not called very often
+        if not self._delta_called_recently(user):
+            # Get delta metadata from dropbox
+            access_token = user.get_dropbox_token()
+            post_args = {}
+            if user.dbox_cursor:
+                post_args['cursor'] = user.dbox_cursor
+            has_more = True
+            cursor = None
+            while has_more:
+                # make dropbox request
+                user.last_delta = datetime.now()
+                # TODO find a way to call save one time in this func
+                yield motor.Op(user.save, self.db)
+                response = yield gen.Task(self.dropbox_request,
+                    "api", "/1/delta",
+                    access_token=access_token,
+                    post_args=post_args)
+                if self._check_bad_response(response, callback):
+                    return
+                dbox_delta = json.loads(response.body)
+                has_more = dbox_delta['has_more']
+                cursor = dbox_delta['cursor']
+                if dbox_delta['reset']:
+                    logger.debug(
+                        "Reseting user all files for '{0}'".format(user.name))
+                    yield motor.Op(self.db[user.name].drop)
+                for e_path, entry in dbox_delta['entries']:
+                    if entry is None:
+                        # TODO
+                        # maybe there is a way to delete files in one db call
+                        yield motor.Op(
+                            DropboxFile.remove_entries, self.db,
+                            {"_id": e_path}, collection=user.name)
+                    else:
+                        # TODO
+                        # maybe there is a way to save files in one db call
+                        yield motor.Op(self._save_meta, entry, user.name, False)
+            user.dbox_cursor = cursor
+            yield motor.Op(user.save, self.db)
+        callback({'status': ErrCode.ok})
+
+    def _get_response_encoding(self, response):
+        encoding = 'ascii'
+        cont_type = response.headers.get('Content-Type', None)
+        if cont_type:
+            for v in cont_type.split(';'):
+                if 'charset=' in v:
+                    encoding = v.split('=', 1)[-1]
+                    encoding = DROPBOX_ENCODE_MAP.get(encoding, encoding)
+                    break
+        return encoding
+
+    def _check_bad_response(self, response, callback=None):
+        if response.code != 200:
+            error = 'undefined'
+            if 'error' in response.body:
+                error = json.loads(response.body)['error']
+            result = {
+                "status": ErrCode.bad_request,
+                'http_code': response.code,
+                'error': error,
+            }
+            if callback:
+                callback(result)
+            return result
+        return None
+
+    def _get_file_url(self, path, api_url):
+        path = self._unify_path(path)
+        return "/1/{api_url}/{{root}}{path}".format(
+            api_url=api_url,
+            path=urllib.quote(path.encode('utf8')))
+
+    def _skip_spam_filemeta(self, file_meta, callback):
+        if file_meta and file_meta.last_updated:
+            time_left = datetime.now() - file_meta.last_updated
+            if time_left.total_seconds() < FILE_CONTENT_PERIOD_SEC:
+                callback({
+                    'status': ErrCode.called_too_often})
+                return True
+        return False
+
+    @gen.engine
+    def _save_meta(self, meta_data, colln, update=True,
+      callback=None):
+        meta_data['_id'] = meta_data.pop('path')
+        meta_data['root_path'] = os.path.dirname(meta_data['_id'])
+        if update:
+            meta_data['last_updated'] = datetime.now()
+        self.db[colln].save(meta_data, callback=callback)
