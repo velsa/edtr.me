c740c80dd5d72d46ee4892b5df91ecdc55984211
st4lk <myhappydo@gmail.com>
2013-03-10 12:05:23 +0400

    test, that meta is updated on get_file

--- edtr.me/tests/dbox_api_ajax_test.py
+++ edtr.me/tests/dbox_api_ajax_test.py
@@ -186,6 +186,8 @@ class GetContentTest(BaseTest):
     def test_text_content(self, m_fetch, m_get_current_user):
         self.create_test_user(m_get_current_user)
         self.text_content = 'text text text'
+        self.dbox_path = '/1.txt'
+        self.updated_revision = 100
 
         meta = json.loads(get_dbox_meta('/1.txt', TF.text, with_path=False))
         meta_db, _ = _adopt_meta(meta, separate_id=False)
@@ -195,8 +197,10 @@ class GetContentTest(BaseTest):
             if not isinstance(request, HTTPRequest):
                 request = HTTPRequest(url=request, **kwargs)
             request.headers = HTTPHeaders(request.headers)
-            if "api-content.dropbox.com/1/files/" in request.url:
+            if "api-content.dropbox.com/1/files/" in request.url and\
+              self.dbox_path in request.url:
                 dbox_resp = self.text_content
+                meta['revision'] = self.updated_revision
                 headers = {'X-Dropbox-Metadata': json.dumps(meta)}
             else:
                 self.assertTrue(False)
@@ -210,7 +214,7 @@ class GetContentTest(BaseTest):
         m_fetch.side_effect = fetch_mock
 
         response = self.post_with_xsrf(
-            self.reverse_url('dropbox_get_file'), {'path': '/1.txt'})
+            self.reverse_url('dropbox_get_file'), {'path': self.dbox_path})
         self.assertEqual(response.code, 200)
         json_resp = json.loads(response.body)
         self.assertEqual(json_resp['errcode'], 0)
@@ -220,3 +224,4 @@ class GetContentTest(BaseTest):
             "modified", "client_mtime", "root_path", "is_dir", "icon", "root",
             "mime_type", "bytes", "size"])
         self.assertEqual(file_meta_params, set(json_resp['meta'].keys()))
+        self.assertEqual(json_resp['meta']['revision'], self.updated_revision)
